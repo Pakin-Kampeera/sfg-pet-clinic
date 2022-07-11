9c6324b4db57be749dbffa9fa3e80fe4b03a040a
Pakin-Kampeera <armpakin@hotmail.com>
2022-07-11 16:19:23 +0700

    Update: Enitity and Controller

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.service.OwnerService;
+import guru.springframework.sfgpetclinic.service.PetTypeService;
 import guru.springframework.sfgpetclinic.service.VetService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -16,6 +18,7 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Override
     public void run(String... args) {
@@ -23,15 +26,33 @@ public class DataLoader implements CommandLineRunner {
     }
 
     private void loadData() {
+        PetType dog = new PetType();
+        dog.setName("dog");
+
+        petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("cat");
+
+        petTypeService.save(cat);
+
+        log.info("Loaded PetType...");
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michel");
         owner1.setLastName("Winston");
+        owner1.setAddress("123 London");
+        owner1.setCity("1234");
+        owner1.setTelephone("08974512");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Stephen");
         owner2.setLastName("Elv");
+        owner1.setAddress("123 Helsinki");
+        owner1.setCity("5647");
+        owner1.setTelephone("09547512");
 
         ownerService.save(owner2);
 
